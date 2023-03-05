f80131d9779b9469ee276da214538638349c596c
Simon Weald <glitchcrab-github@simonweald.com>
2023-03-05 15:57:24 +0000

    switch templating method

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+data "template_file" "userdata" {
+  template = file("${path.module}/templates/userdata.tpl")
+
+  vars {
+    user_data_blob = var.user_data_blob
+  }
+}
+
 resource "null_resource" "cloudinit_userdata" {
   connection {
     type        = var.conn_type
@@ -7,10 +15,19 @@ resource "null_resource" "cloudinit_userdata" {
   }
 
   provisioner "file" {
-    content = templatefile("${path.module}/templates/userdata.tpl", {
-      user_data_blob = var.user_data_blob
-    })
+    content     = data.template_file.userdata.rendered
     destination = "${var.snippet_root_dir}/${var.snippet_dir}/user-${var.snippet_file_base}"
+  }
+}
+
+data "template_file" "network" {
+  template = file("${path.module}/templates/network.tpl")
+
+  vars {
+    primary_network = var.primary_network
+    extra_networks  = var.extra_networks != null ? var.extra_networks : []
+    search_domains  = var.search_domains
+    dns_servers     = var.dns_servers
   }
 }
 
@@ -23,12 +40,7 @@ resource "null_resource" "cloudinit_network" {
   }
 
   provisioner "file" {
-    content = templatefile("${path.module}/templates/network.tpl", {
-      primary_network = var.primary_network
-      extra_networks  = var.extra_networks != null ? var.extra_networks : []
-      search_domains  = var.search_domains
-      dns_servers     = var.dns_servers
-    })
+    content     = data.template_file.network.rendered
     destination = "${var.snippet_root_dir}/${var.snippet_dir}/network-${var.snippet_file_base}"
   }
 }
